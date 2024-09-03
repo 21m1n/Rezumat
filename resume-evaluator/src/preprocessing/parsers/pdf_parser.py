df11a91ffdd10ed6b486e36d5d1fa593a9bca755
Zimin <z.luo@student.uw.edu.pl>
2024-09-03 19:02:43 +0800

    refactoring

--- resume-evaluator/src/preprocessing/parsers/pdf_parser.py
+++ resume-evaluator/src/preprocessing/parsers/pdf_parser.py
@@ -4,25 +4,21 @@ import os
 from tqdm import tqdm
 from pypdf import PdfReader
 from pathlib import Path
+from typing import Union, List
 
-pdf_path = Path("data/resumes")
-os.makedirs(pdf_path, exist_ok=True)
 
-num_threads = os.cpu_count()
-total_pdfs = len(list(pdf_path.glob("*.pdf")))
-cv_data = []
-
-def process_pdf(file_path):
+def parse_pdf(file_path):
     reader = PdfReader(file_path)
     return [page.extract_text() for page in reader.pages]
 
+def process_pdfs(pdf_path: Union[str, Path]) -> List[str]:
+    cv_data = []
+    total_pdfs = len(list(Path(pdf_path).glob("*.pdf")))
+    with ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
+        future_to_file = {executor.submit(parse_pdf, file): file for file in pdf_path.glob("*.pdf")}
+        
+        for future in tqdm(as_completed(future_to_file), total=total_pdfs, desc="Parsing PDFs"):
+            cv_data.append(future.result())
 
-
-with ThreadPoolExecutor(max_workers=num_threads) as executor:
-    future_to_file = {executor.submit(process_pdf, file): file for file in pdf_path.glob("*.pdf")}
-    
-    for future in tqdm(as_completed(future_to_file), total=total_pdfs):
-        cv_data.append(future.result())
-
-# Join multi-page CVs into a single string
-cv_data = [" ".join(cv) if len(cv) > 1 else cv[0] for cv in cv_data]
+    # Join multi-page CVs into a single string
+    return [" ".join(cv) if len(cv) > 1 else cv[0] for cv in cv_data]
