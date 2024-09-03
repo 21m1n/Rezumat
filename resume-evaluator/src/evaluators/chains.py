1adc009bc0ee90bc21d7a6c5a3374cab8b7fc0d8
Zimin <z.luo@student.uw.edu.pl>
2024-09-03 13:50:40 +0800

    added chains module

---
+++ resume-evaluator/src/evaluators/chains.py
@@ -0,0 +1,46 @@
+from langchain_openai import ChatOpenAI
+from langchain_groq import ChatGroq
+from langchain_anthropic import ChatAnthropic
+from langchain_ollama import ChatOllama
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.language_models.chat_models import BaseChatModel
+from ..prompts import TWO_STAGE_EVAL_JD_PROMPT, TWO_STAGE_EVAL_CV_PROMPT
+
+def get_model(model_text: str, model_id: str, temperature: float=0, max_tokens: int=2048):
+    """get model based on the input data"""
+
+    if model_text == "groq":
+      model = ChatGroq(model=model_id, temperature=temperature, max_tokens=max_tokens)
+    elif model_text == "OpenAI":
+      model = ChatOpenAI(model=model_id, temperature=temperature, max_tokens= max_tokens)
+    elif model_text == "Anthropic":
+      model = ChatAnthropic(model=model_id, temperature=temperature, max_tokens=max_tokens)
+    elif model_text == "Ollama":
+      model = ChatOllama(model=model_id, temperature=temperature, max_tokens=max_tokens)
+      
+    return model
+  
+def get_eval_chain(model_text: str, model_id: str, type: str):
+
+  model_text = model_text.lower()
+
+  model = get_model(model_text, model_id)
+  
+  if type == "jd":
+    eval_prompt = PromptTemplate(
+      input_variables=["job_description"],
+      template=TWO_STAGE_EVAL_JD_PROMPT
+      )
+  elif type == "cv":
+    eval_prompt = PromptTemplate(
+      input_variables=["job_description", "resume"],
+      template=TWO_STAGE_EVAL_CV_PROMPT
+      )
+  else:
+    raise ValueError("Invalid type")
+    
+  grader = eval_prompt | model | JsonOutputParser()
+  model_tuples = (model_text.lower(), model)
+    
+  return grader, model_tuples
