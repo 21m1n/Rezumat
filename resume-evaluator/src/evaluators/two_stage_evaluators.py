7f4dcbd52c0a91ebae5a17e51c76c41b1cdddd22
Zimin <z.luo@student.uw.edu.pl>
2024-09-03 19:01:37 +0800

    added evaluators for cv files

--- resume-evaluator/src/evaluators/two_stage_evaluators.py
+++ resume-evaluator/src/evaluators/two_stage_evaluators.py
@@ -1,6 +1,23 @@
-# evaluate resume 
-def two_stage_eval_jd(model_tuples: List[Tuple[str, RunnableSequence]], job_description: str, job_id: str, output_dir: str) -> Union[pd.DataFrame, None]:
+import json
+import logging
+import os
+import time
+from pathlib import Path
+from typing import Dict, List, Tuple, Union
+
+import pandas as pd
+from langchain_core.runnables import RunnableSequence
+from tqdm import tqdm
+
+
+def two_stage_eval_jd(model_tuples: List[Tuple[str, RunnableSequence]], job_tuple: Tuple[str, str], output_dir: str) -> Union[pd.DataFrame, None]:
     model_results = {}
+    
+    if isinstance(model_tuples, Tuple):
+        model_tuples = [model_tuples]
+    
+    job_id, job_description = job_tuple
+    
     for model_name, grader in model_tuples:
         try:
             result = grader.invoke({"job_description": job_description})
@@ -10,6 +27,7 @@ def two_stage_eval_jd(model_tuples: List[Tuple[str, RunnableSequence]], job_desc
             json_file = os.path.join(output_dir, f"{job_id}_{model_name}.json")
             with open(json_file, "w") as f:
                 json.dump(result, f, indent=4)
+                logging.info(f"Saved {model_name} result for job_id: {job_id}")
             time.sleep(2.1)  # Add a small delay to avoid rate limiting
 
         except Exception as e:
@@ -23,8 +41,15 @@ def two_stage_eval_jd(model_tuples: List[Tuple[str, RunnableSequence]], job_desc
         print(error_msg)
         return None
 
-def two_stage_eval_cv(model_tuples: List[Tuple[str, RunnableSequence]], job_requirements: str, job_id: str, cv: str, cv_id: str, output_dir: str) -> Union[pd.DataFrame, None]:
+def two_stage_eval_cv(model_tuples: List[Tuple[str, RunnableSequence]], job_tuple: Tuple[str, str], cv_tuple: Tuple[str, str], output_dir: str) -> Union[pd.DataFrame, None]:
     model_results = {}
+    
+    if isinstance(model_tuples, Tuple):
+        model_tuples = [model_tuples]   
+        
+    job_id, job_requirements = job_tuple
+    cv_id, cv = cv_tuple
+    
     for model_name, grader in model_tuples:
         try:
             result = grader.invoke({"job_requirements": job_requirements, "resume": cv})
@@ -47,5 +72,63 @@ def two_stage_eval_cv(model_tuples: List[Tuple[str, RunnableSequence]], job_requ
         print(error_msg)
         return None
 
-def evaluate_resumes():
-  pass
+def resume_evaluation(eval_results_folder: str) -> pd.DataFrame:
+
+    results = []
+    errors = []
+
+    for file in tqdm(Path(eval_results_folder).glob("*.json"), total=len(list(Path(eval_results_folder).glob("*.json"))), desc="Evaluating results"):
+        try:
+            file_name = file.stem
+            job_id, cv_id, model_name = file_name.split("_")
+            with open(file, "r") as f:
+                result = json.load(f)
+            
+            data = {
+                "job_id": job_id,
+                "cv_id": cv_id,
+                "model_name": model_name,
+                "original_technical_skills": result["resume_evaluation"]["original_scores"].get("technical_skills", None),
+                "original_soft_skills": result["resume_evaluation"]["original_scores"].get("soft_skills", None),
+                "original_experience": result["resume_evaluation"]["original_scores"].get("experience", None),
+                "original_education": result["resume_evaluation"]["original_scores"].get("education", None),
+                "recalibrated_technical_skills": result["recalibrated_scores"].get("technical_skills", None),
+                "recalibrated_soft_skills": result["recalibrated_scores"].get("soft_skills", None),
+                "recalibrated_experience": result["recalibrated_scores"].get("experience", None),
+                "recalibrated_education": result["recalibrated_scores"].get("education", None),
+                "inferred_experience": ", ".join(result["deeper_analysis"].get("inferred_experience", [])),
+                "suitability": result["assessment"].get("suitability", None),
+                "strengths": result["assessment"].get("strengths", None),
+                "concerns": result["assessment"].get("concerns", None)
+            }
+            
+            results.append(data)
+        except Exception as e:
+            errors.append({"file": str(file), "error": str(e)})
+            print(f"Error processing {file}: {e}")
+
+    # Convert results to a DataFrame
+    df = pd.DataFrame(results)
+    
+    return df
+
+def calculate_fit_scores(eval_results_folder: str, weights: Dict[str, float]) -> pd.DataFrame:
+
+    df = resume_evaluation(eval_results_folder)
+    
+    weights = {
+    "technical_skills": weights.technical_skills,
+    "soft_skills": weights.soft_skills,
+    "experience": weights.experience,
+    "education": weights.education
+    }
+    
+    # Define score types
+    score_types = ["original", "recalibrated"]
+    
+    # Calculate overall scores using pandas' dot product
+    for score_type in score_types:
+        columns = [f"{score_type}_{skill}" for skill in weights.keys()]
+        df[score_type+"_overall_score"] = df[columns].values.dot(pd.Series(weights).values)
+    
+    return df
